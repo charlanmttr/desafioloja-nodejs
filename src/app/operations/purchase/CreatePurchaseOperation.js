449f4acd054fdfa617f8403996ca649b1693f401
Charlan Oliveira Matter <mattergremio2@gmail.com>
2020-10-26 18:31:28 -0300

    fix: changes in CreatePurchaseOperation and PurchaseFactory

--- src/app/operations/purchase/CreatePurchaseOperation.js
+++ src/app/operations/purchase/CreatePurchaseOperation.js
@@ -9,39 +9,37 @@ module.exports = ({ purchaseService, productService, purchaseFactory, logger }) 
             } = body;
 
             const productReceived = await productService.find(productId);
-            if (!productReceived) throw ('product not found');
+            if (!productReceived) 
+                throw ('product not found');
 
             const {
                 amount: dbAmount,
                 valueUnitary: dbValueUnitary
             } = productReceived;
 
-            if (dbAmount === 0) throw ('product out of stock');
-
+            if (dbAmount === 0) 
+                throw ('product out of stock');
+            
+            let purchasePayload;
+            
             if (numberOfInstallments > 6) {
-                const amountToPay = dbValueUnitary - inputValue;
-                const rate = 2.05;
-
-                const amountWithInterest = amountToPay * (1 + (rate / 100)) ** numberOfInstallments;
-                const installmentValue = amountWithInterest / numberOfInstallments;
-                const totalToPay = amountWithInterest + inputValue;
-
-                console.log(`COM JUROS: O valor da primeira parcela será de ${inputValue} e as ${numberOfInstallments} restantes serão no valor de ${installmentValue.toFixed(2)} reais. Resultando no total de ${totalToPay.toFixed(2)} reais.`);
-
-                let purchasePayload = purchaseFactory.toPurchaseDatabase(productId, inputValue, numberOfInstallments, installmentValue, totalToPay, rate);
-                
-                return purchaseService.create(purchasePayload);
+                purchasePayload = purchaseFactory.calculateValueWithInterest(
+                    productId,
+                    dbValueUnitary,
+                    inputValue,
+                    numberOfInstallments
+                );
+  
             } else {
-                const amountToPay = dbValueUnitary - inputValue;
-
-                const installmentValue = amountToPay / numberOfInstallments;
-                console.log(`SEM JUROS: O valor das parcelas será de ${installmentValue} reais.`);
-
-                let purchasePayload = purchaseFactory.toPurchaseDatabase(productId, inputValue, numberOfInstallments, installmentValue, dbValueUnitary);
-
-                return purchaseService.create(purchasePayload);
+                purchasePayload = purchaseFactory.calculateValueWithoutInterest(
+                    productId,
+                    dbValueUnitary,
+                    inputValue,
+                    numberOfInstallments
+                );
             }
 
+            return purchaseService.create(purchasePayload);
         } catch (error) {
             logger.error(error);
             throw error;
--- src/domain/services/purchase/PurchaseFactory.js
+++ src/domain/services/purchase/PurchaseFactory.js
@@ -1,17 +1,41 @@
 module.exports = () => ({
-    toPurchaseDatabase: ( product, inputValue, numberOfInstallments, installmentValue, totalToPay, monthlyInterestRate = undefined ) => {
-        
-        const purchase = {
+
+    calculateValueWithInterest: (product, dbValueUnitary, inputValue, numberOfInstallments) => {
+        const amountToPay = dbValueUnitary - inputValue;
+        const rate = 2.05;
+
+        const amountWithInterest = amountToPay * (1 + (rate / 100)) ** numberOfInstallments;
+        const installmentValue = amountWithInterest / numberOfInstallments;
+        const totalToPay = amountWithInterest + inputValue;
+    
+        console.log(`COM JUROS: O valor da primeira parcela será de ${inputValue} e as ${numberOfInstallments} restantes serão no valor de ${installmentValue.toFixed(2)} reais. Resultando no total de ${totalToPay.toFixed(2)} reais.`);
+    
+        return {
             product,
             paymentCondition: {
                 inputValue,
                 numberOfInstallments
             },
-            monthlyInterestRate,
+            monthlyInterestRate: rate,
             installmentValue,
             value: totalToPay
         };
+    },
 
-        return purchase;
+    calculateValueWithoutInterest: (product, dbValueUnitary, inputValue, numberOfInstallments) => {
+        const amountToPay = dbValueUnitary - inputValue;
+        const installmentValue = amountToPay / numberOfInstallments;
+
+        console.log(`SEM JUROS: O valor das parcelas será de ${installmentValue} reais.`);
+
+        return {
+            product,
+            paymentCondition: {
+                inputValue,
+                numberOfInstallments
+            },
+            installmentValue,
+            value: dbValueUnitary
+        };
     }
 });
